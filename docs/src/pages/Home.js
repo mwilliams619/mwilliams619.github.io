278c67222ea6fdcec4fb948b7334e0a2585c0a7b
Matt Williams <mattwilliams6198@gmail.com>
2024-07-11 13:48:42 -0500

    updated formatting

--- docs/src/pages/Home.js
+++ docs/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css'
+import ProteinViewer from '../components/protein.js';
 import { useBgColor } from '../functions/useBgColor'
 
 function Home() {
@@ -15,19 +16,22 @@ function Home() {
     </p>
     <ul className='no-bullet'>
         <li>
-            🤖 Deployed ML-driven approaches for strain engineering projects
+            🤖 Deployed <strong>ML-driven</strong> approaches for strain engineering projects
         </li>
         <li>
-            💻 Built bioinformatics tools to help synbio startups turn ideas into reality.
+            💻 Built <strong>bioinformatics</strong> tools to help synbio startups turn ideas into reality.
         </li>
         <li>
-            🎓 Studied biology, computer science, and entrepreneurship - because in biotech, you've got to wear many hats!
+            🎓 Studied <strong>biology, computer science, and entrepreneurship</strong> - because in biotech, you've got to wear many hats!
         </li>
     </ul>
 
     <p>
         Passionate about 🧑🏽‍💻 developing software tools to empower scientists and commercial stakeholders to efficiently reach their goals and 📚 continously learning to keep up with the technological innovations occuring around us.
     </p>
+    <div>
+    <ProteinViewer pdb='6tmf' chain='' seq='' />
+    </div>
     </div>
   );
 }
